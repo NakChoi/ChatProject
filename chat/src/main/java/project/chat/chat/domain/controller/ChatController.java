9789bf64b366cdf8db1d996209b9d9fe58666f5b
NakChoi <nakchoi94@gmail.com>
2023-05-31 15:15:46 +0900

    컨트롤러 핸들러 컨피그 설정

--- chat/src/main/java/project/chat/chat/domain/controller/ChatController.java
+++ chat/src/main/java/project/chat/chat/domain/controller/ChatController.java
@@ -1,4 +1,20 @@
 package project.chat.chat.domain.controller;
 
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@Log4j2
 public class ChatController {
+
+    @GetMapping("/chat")
+    public String chatGet() {
+
+        log.info("@ChatController, chat GET()");
+
+        return "chat";
+    }
 }
--- chat/src/main/java/project/chat/chat/domain/websocket/WebSocketConfig.java
+++ chat/src/main/java/project/chat/chat/domain/websocket/WebSocketConfig.java
@@ -1,4 +1,23 @@
 package project.chat.chat.domain.websocket;
 
-public class WebSocketConfig {
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSocket // @EnableWebSocket 어노테이션을 사용해 WebSocket을 활성화
+public class WebSocketConfig implements WebSocketConfigurer { // 핸들러를 이용해 WebSocket을 활성화하기 위한 Config
+
+    private final ChatHandler chatHandler;
+
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(chatHandler, "ws/chat").setAllowedOrigins("*");
+        // WebSocket에 접속하기 위한 Endpoint는 /chat으로 설정
+    }
 }
